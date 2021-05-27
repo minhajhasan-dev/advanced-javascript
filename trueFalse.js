1aba1ba6086b0058ed0b166811827d5235751af8
minhajhasan-dev <minhajhasan.dev@gmail.com>
2021-05-27 07:31:42 +0600

    true vs false

---
+++ trueFalse.js
@@ -0,0 +1,25 @@
+// Falsy:
+// 0
+// ""
+// undefined
+// null 
+// NaN
+// false 
+
+// Truthy:
+// '0'
+// " "
+// []
+// {}
+
+
+
+// const age = 10;
+
+let name = NaN;
+console.log(name)
+if (name) {
+  console.log("condition is true");
+} else {
+  console.log("condition is false");
+}
