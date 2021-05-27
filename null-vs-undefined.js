de62adb6d526267d3a3b1f58064544f044acde98
minhajhasan-dev <minhajhasan.dev@gmail.com>
2021-05-27 07:53:23 +0600

    null vs undefined concept cleared for interview

--- null-vs-undefined.js
+++ null-vs-undefined.js
@@ -1,21 +1,21 @@
-// let pakhi;
+let pakhi;
 
-// function add (num1, num2){
-//     console.log(num1+num2);
-//     return
-// }
+function add (num1, num2){
+    console.log(num1+num2);
+    return
+}
 
-// function add(num1, num2) {
-//   console.log(num1, num2);
-// }
+function add(num1, num2) {
+  console.log(num1, num2);
+}
 
-// const premik = {name:"smart dude", phone:243535};
-// console.log(premik.gf);
+const premik = {name:"smart dude", phone:243535};
+console.log(premik.gf);
 
-// let fun = undefined;
-// console.log(fun);
+let fun = undefined;
+console.log(fun);
 
-// let ages = [2,5,6,7]
-// console.log(ages[12]);
+let ages = [2,5,6,7]
+console.log(ages[12]);
 
-// null  = nothing here ; value = 0;
+null  = nothing here ; value = 0;
